b105ddfe093f2b23dc8bbbc23578b8714a632a35
iamdoky <kdh6631@gmail.com>
2025-03-24 20:22:16 +0900

    test : Kotlin Practice Test Code

---
+++ src/test/java/com/books/kotlin/practice/KotlinPracticeTestCode.kt
@@ -0,0 +1,15 @@
+package com.books.kotlin.practice
+
+import org.junit.jupiter.api.Test
+
+class KotlinPracticeTestCode {
+
+    private val numbers: List<Int> = listOf(1, 2, 3, 4, 5)
+    private val evenNumber = numbers.filter { it % 2 == 0 }
+
+
+    @Test
+    fun test() {
+        println(evenNumber)
+    }
+}
